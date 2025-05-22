ec245d75b7b09fe989fa2522ae4d786de415207a
Sergey Agarkov <grey1701@gmail.com>
2025-05-22 20:51:31 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,83 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+// let int = -3;
+// if (int > 0) {
+//   console.log(true);
+// } else if (int < 0) {
+//   console.log(false);
+// } else console.log('0 не може бути більше або менше 0');
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',"qwerty", true
+// let variable = true;
+// if (variable === 'test') {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+// let num = 13;
+// if (num > 10) {
+//   num -= 5;
+// } else {
+//   num += 5;
+// }
+// console.log(num);
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+// let numberOfMonth = 13;
+// let nameOfMonth;
+// switch (numberOfMonth) {
+//   case 1:
+//     nameOfMonth = 'Jan';
+//     break;
+//   case 2:
+//     nameOfMonth = 'Feb';
+//     break;
+//   case 3:
+//     nameOfMonth = 'Mar';
+//     break;
+//   case 4:
+//     nameOfMonth = 'Apr';
+//     break;
+//   case 5:
+//     nameOfMonth = 'May';
+//     break;
+//   case 6:
+//     nameOfMonth = 'Jun';
+//     break;
+//   case 7:
+//     nameOfMonth = 'Jul';
+//     break;
+//   case 8:
+//     nameOfMonth = 'Aug';
+//     break;
+//   case 9:
+//     nameOfMonth = 'Sep';
+//     break;
+//   case 10:
+//     nameOfMonth = 'Oct';
+//     break;
+//   case 11:
+//     nameOfMonth = 'Nov';
+//     break;
+//   case 12:
+//     nameOfMonth = 'Dec';
+//     break;
+//   default:
+//     nameOfMonth = 'Error! we have only 12 months';
+//     break;
+// }
+// console.log(nameOfMonth);
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+// let num = 138;
+// let n1 = Math.floor(num / 100);
+// let n2 = Math.floor((num % 100) / 10);
+// let n3 = Math.floor((num % 100) % 10);
+// console.log(num, n1, n2, n3, n1 + n2 + n3);
